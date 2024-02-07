850f72a1718c2f58b67085b250f675c95310f5b5
g.mezentsev <g.mezentsev@an02.zhores>
2024-02-07 11:29:17 +0300

    finilize mrpc, add qnli and rte optimal hyperparams, fix processing script

--- sparse_optimizers/process_results.py
+++ sparse_optimizers/process_results.py
@@ -4,8 +4,9 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    tasks =  ['cola', 'mnli', 'mrpc', 'qnli', 'qqp', 'rte', 'sst2', 'stsb', 'wnli']
-    random_seeds = [42, 3705, 2023, 7, 3612]
+    tasks = ['cola',]
+    # tasks =  ['cola', 'mnli', 'mrpc', 'qnli', 'qqp', 'rte', 'sst2', 'stsb', 'wnli']
+    random_seeds = [42, 3705, 2023]
 
     task2metric_for_best_model = {
         'cola': 'matthews_correlation',
@@ -19,17 +20,26 @@ if __name__ == '__main__':
         'wnli': 'accuracy'
     }
 
-    log_dir = './logs/lora'
+    log_dir = './logs'
 
-    metrics = {}
 
-    for task in tasks:
-        log_file = os.path.join(log_dir, f'{task}.json')
+    for model_path in ['roberta-base', 'bert-base-uncased']:
+        print('\n\n', model_path)
+        for run_type in ['ft', 'lora', 'sparse']:
+            metrics = {}
 
-        with open(log_file, 'r') as f:
-            logs = json.load(f)
+            for task in tasks:
+                log_file = os.path.join(log_dir, run_type, f'{model_path}_{task}.json')
+                
+                if os.path.exists(log_file):
+                    with open(log_file, 'r') as f:
+                        logs = json.load(f)
 
-        metrics[task] = np.mean([logs[str(seed)]['val'][f'eval_{task2metric_for_best_model[task]}'] for seed in random_seeds])
+                    try:
+                        metrics[task] = np.mean([logs[str(seed)]['val'][f'eval_{task2metric_for_best_model[task]}'] for seed in random_seeds])
+                    except:
+                        pass
 
-    print(metrics)
-    print(np.mean(list(metrics.values())))
+            print(run_type)
+            print(metrics)
+            print(np.mean(list(metrics.values())))
