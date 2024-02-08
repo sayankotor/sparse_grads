9e488425f17e3a16eb7661e3da837815db1ad04a
Gleb Mezentsev <g.mezentsev@skoltech.ru>
2024-02-08 12:28:46 +0300

    add copy of a final db with optimal hyperparams

--- sparse_optimizers/merge_dbs.py
+++ sparse_optimizers/merge_dbs.py
@@ -1,14 +1,12 @@
 import optuna
 
+for model_path in ['bert-base-uncased', 'roberta-base']:
+    for task in ['cola', 'mnli', 'mrpc', 'qnli', 'qqp', 'rte', 'sst2', 'stsb', 'wnli']:
+        for run_type in ['ft', 'lora', 'sparse']:
+            study = f'{task}_{run_type}_{model_path}'
 
-for study in ['mnli_ft_bert-base-uncased', 'sst2_ft_bert-base-uncased', 'sst2_lora_roberta-base',
-              'mnli_lora_bert-base-uncased', 'sst2_lora_bert-base-uncased', 'sst2_sparse_bert-base-uncased',
-              'sst2_sparse_roberta-base', 'qqp_sparse_roberta-base', 'mnli_lora_roberta-base', 'mnli_sparse_roberta-base',
-              'qqp_lora_roberta-base', 'mnli_sparse_bert-base-uncased', 'qqp_sparse_bert-base-uncased', 'qqp_lora_bert-base-uncased']:
-
-
-    optuna.copy_study(
-        from_study_name=study,
-        from_storage="sqlite:///sparse-grad.db",
-        to_storage=f"postgresql+psycopg2://sparse-grad:mooKah4i@doge.skoltech.ru/sparse-grad",
-    )
+            optuna.copy_study(
+                from_study_name=study,
+                from_storage="postgresql+psycopg2://sparse-grad:mooKah4i@doge.skoltech.ru/sparse-grad",
+                to_storage="sqlite:///sparse-grad_final.db"
+            )
